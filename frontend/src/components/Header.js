214d40a0b05117daf7bbcd6ca274705dca3f58c7
matthew croning <matthewcroning@outlook.com>
2022-06-11 15:43:52 +1000

    completed basic frontedn

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 const Header = () => {
   return (
     <header>
         <Navbar bg="light" expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="#">Iconic</Navbar.Brand>
+                <LinkContainer to="/">
+                    <Navbar.Brand>Iconic</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                 <Nav className="ml-auto">
-                    <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                <LinkContainer to="/cart">
+                    <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
                     <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+                </LinkContainer>
 
                 </Nav>
                 </Navbar.Collapse>
